061f26bfe5c6b294e5147a23757a13181126f807
Mark Solonchev <solonchev32@gmail.com>
2024-03-31 22:28:52 +0300

    init project for login-registration

---
+++ login-registration/src/test/java/ru/solonchev/loginregistration/LoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.solonchev.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
