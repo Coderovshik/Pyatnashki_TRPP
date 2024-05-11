64f3c73056601999a248d4cec30a2e69dafe1d66
Coderovshik <peter.04007@gmail.com>
2024-05-11 17:16:25 +0300

    go migrator fix

--- migrator/migrations/000001_create_users_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id SERIAL PRIMARY KEY,
-    email VARCHAR UNIQUE NOT NULL,
-    user_password VARCHAR NOT NULL
-);
--- migrator/migrations/000002_insert_users_table.up.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users(email, user_password)
-VALUES
-    ('email1@example.com', '1234'),
-    ('email2@example.com', '5678'),
-    ('email3@example.com', '9101');
